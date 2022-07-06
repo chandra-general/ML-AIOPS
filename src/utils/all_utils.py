2b736dccb39846a4da982cc04a21385d5871aea0
Chandra Reddy Tiyyagura <chandra.tiyyagura@gmail.com>
2022-07-06 22:39:48 +0530

    Stage 01 created

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -10,3 +10,11 @@ def read_yaml(path_to_yaml:str) -> dict:
             return yaml.safe_load(yaml_file)
         except yaml.YAMLError as exc:
             print(exc)
+
+def create_directory(dirs: list):
+    """
+    Creates a directory if it doesn't exist
+    """
+    for dir_path in dirs:
+        os.makedirs(dir_path, exist_ok=True)
+        print(f"Directory is created at {dir_path}")
