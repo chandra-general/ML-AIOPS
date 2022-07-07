82ece2bf5ad07cee5d811ee2b34af383663e4423
Chandra Reddy Tiyyagura <chandra.tiyyagura@gmail.com>
2022-07-07 09:40:19 +0530

    split train and evaluate files added

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -1,20 +1,27 @@
 import yaml
 import os
+import json
 
-def read_yaml(path_to_yaml:str) -> dict:
-    """
-    Reads a yaml file and returns a dictionary
-    """
-    with open(path_to_yaml, 'r') as yaml_file:
-        try:
-            return yaml.safe_load(yaml_file)
-        except yaml.YAMLError as exc:
-            print(exc)
+
+def read_yaml(path_to_yaml: str) -> dict:
+    with open(path_to_yaml) as yaml_file:
+        content = yaml.safe_load(yaml_file)
+
+    return content
+
 
 def create_directory(dirs: list):
-    """
-    Creates a directory if it doesn't exist
-    """
     for dir_path in dirs:
         os.makedirs(dir_path, exist_ok=True)
-        print(f"Directory is created at {dir_path}")
+        print(f"directory is created at {dir_path}")
+
+
+def save_local_df(data, data_path, index_status=False):
+    data.to_csv(data_path, index=index_status)
+    print(f"data is saved at {data_path}")
+
+
+def save_reports(report: dict, report_path: str, indentation=4):
+    with open(report_path, "w") as f:
+        json.dump(report, f, indent=indentation)
+    print(f"reports are saved at {report_path}")
